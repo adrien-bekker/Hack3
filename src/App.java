0af9a8cd33ff6effabd32c1101cf774634206002
Drin <adrien.bekker@gmail.com>
2020-07-11 15:38:52 -0400

    Adding Basic Visual Structure
    Created FXML and loads up on start

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
-public class App {
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    public void start(Stage primaryStage) {
+        try {
+            // Loads up application
+            Parent root = FXMLLoader.load(getClass().getResource("Guest.fxml"));
+            Scene scene = new Scene(root, 900, 600);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            // If there is an error it prints it and stop the program
+            System.out.println(e);
+            System.exit(404);
+        }
+    }
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        launch(args);
     }
 }
